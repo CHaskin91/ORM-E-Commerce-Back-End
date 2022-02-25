5ea9fa5861176ddabcd61e68102b8372e8218208
Chubbs91 <chaskin1991@outlook.com>
2022-02-24 19:17:02 -0600

    index.js model created

--- models/index.js
+++ models/index.js
@@ -1,6 +1,35 @@
 // Import Models
 const Category = require('./Category');
+const Product = require('./Product');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
+// Defines a Category association starting with a Product
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+// Defines a Category having many products to create multiple foreign keys in the 'Product' table
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+});
+
+// Product is associated with many Tags
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id',
+});
+
+// Tags associated with many Products
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id',
+});
+
+// Package the four models and export them as an object
 module.exports = {
-    Category
+    Category,
+    Product,
+    Tag,
+    ProductTag,
 };
